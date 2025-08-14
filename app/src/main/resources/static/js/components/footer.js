a26c8994aa8a2a2c7ad052e9e1e84de38f96971f
isumianw <isumianw93@gmail.com>
2025-08-14 01:11:09 -0400

    Fixed footer

--- app/src/main/resources/static/js/components/footer.js
+++ app/src/main/resources/static/js/components/footer.js
@@ -3,32 +3,33 @@ function renderFooter() {
 
     footer.innerHTML = `
         <footer class="footer">
-            <div class="footer-logo">
-                <img src="/assets/images/logo/logo.png" alt="logo">
-                <p>© 2025 Clinic Management System. All rights reserved.</p>
-            </div>
-            <div class="footer-columns">
-                <div class="footer-column">
-                    <h4>Company</h4>
-                    <a href="#>About</h4>
-                    <a href="#">Careers</a>
-                    <a href="#">Press</a>                   
+            <div class="footer-container">
+                <div class="footer-logo">
+                    <img src="/assets/images/logo/logo.png" alt="logo">
+                    <p>© 2025 Clinic Management System. All rights reserved.</p>
                 </div>
-                <div class="footer-column">
-                    <h4>Support</h4>
-                    <a href="#">Account</a>
-                    <a href="#">Help Center</a>
-                    <a href="#>Contact</a>
-                </div>
-                <div class="footer-column">
-                    <h4>Legals</h4>
-                    <a href="#">Terms</a>
-                    <a href="#">Privacy Policy</a>
-                    <a href="#">Licensing</a>
+                <div class="footer-links">
+                    <div class="footer-column">
+                        <h4>Company</h4>
+                        <a href="#">About</a>
+                        <a href="#">Careers</a>
+                        <a href="#">Press</a>                   
+                    </div>
+                    <div class="footer-column">
+                        <h4>Support</h4>
+                        <a href="#">Account</a>
+                        <a href="#">Help Center</a>
+                        <a href="#">Contact</a>
+                    </div>
+                    <div class="footer-column">
+                        <h4>Legals</h4>
+                        <a href="#">Terms</a>
+                        <a href="#">Privacy Policy</a>
+                        <a href="#">Licensing</a>
+                    </div>
                 </div>
             </div>
         </footer>
-            
     `;
 }
 
